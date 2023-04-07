0264116c556ea6f7dcc71eff027cf40adf750426
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-07 11:05:24 -0500

    test

--- neweggs/settings.py
+++ neweggs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ze_g3-ow#tivwe+8pvof#v2m@=c(uue)2(w#yscom1z@)1icl-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
