ad2ae799fc4fd4f70561bbaf3d1493f2a5143b3a
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-10 14:22:49 -0500

    changed models testing paths v4

--- newegg/admin.py
+++ newegg/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Product, ShoppingCart, Category, ProductCategory, OrderDetails
+from .models import Customer, Product, ShoppingCart, Category, ProductCategory, OrderDetails, User
 
 
-
+admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(ShoppingCart)
