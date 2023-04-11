375ecab9e44746e86a3cf161f0f680e873bfce20
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-11 15:20:36 -0500

    changed cart details for auth

--- newegg/views.py
+++ newegg/views.py
@@ -78,8 +78,7 @@ class CategoryList(View):
 
 class CartDetail(View):
     def get(self, request):
-        # customer_id = request.user.customerprofile.id
-        cart_items = ShoppingCart.objects.all
+        cart_items = ShoppingCart.objects.all()
         total_price = sum(item.price for item in cart_items)
         data = {'cart_items': list(cart_items.values()), 'total_price': total_price}
         return JsonResponse(data)
