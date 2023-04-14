c76165e9e677c097bdb3281ea9350bce88e9cecb
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-14 11:39:37 -0500

    test

--- neweggs/urls.py
+++ neweggs/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import views
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newegg.urls')),
-    path('api/customer/login/', views.login_view, name='login'),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
