25bbb51f89bd2d6aa08b171e933433eba330a5da
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-14 09:44:09 -0500

    test

--- neweggs/settings.py
+++ neweggs/settings.py
@@ -33,7 +33,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
