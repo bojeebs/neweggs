12de7dd742daa3c5c067dcc9385ba02f356aa451
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-14 10:20:37 -0500

    test

--- newegg/admin.py
+++ newegg/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Product, ShoppingCart, OrderDetails
+from .models import Product, ShoppingCart, OrderDetails, Customer
 
-
+admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(ShoppingCart)
 admin.site.register(OrderDetails)
