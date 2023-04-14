15cb6007fc7e22cfd588705edb41e51c508db4a4
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-04-14 12:10:17 -0500

    test

--- newegg/urls.py
+++ newegg/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from . import views
 from .views import CreateCustomerView
-
+from newegg.views import login_view
 
 
 urlpatterns = [
      
-     # path('customer/<int:pk>/', views.UserList.as_view(), name='customer_list'),
      path('api/customer/create/', CreateCustomerView.as_view(), name='customer'),
      path('product/', views.ProductList.as_view(), name='products'),
-     path('api/customer/login/', views.LoginView, name='login'),
+     path('api/customer/login/', login_view.as_view(), name='login'),
      # path('cart/<int:customer_id>/', views.CartDetail.as_view(), name='cart'),
      path('cart/remove/<int:product_id>/', views.CartRemove.as_view(), name='remove_cart'),
      path('cart/add/<int:product_id>/', views.CartAdd.as_view(), name='cart_add'),
